2e30f7b6cf152d0f3ac9253db4fe1a8d4809d390
Taketomo Ohishi <takeweb@mac.com>
2024-02-24 19:57:22 +0900

    clap 4.5.1対応

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,30 @@
-use clap::{App, Arg};
+use clap::Parser;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-#[derive(Debug)]
-pub struct Config {
+/// Rust cat CLI
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
+
+    /// Number lines
+    #[arg(short = 'n', long = "number", required = false)]
     number_lines: bool,
+
+    /// Number nonblank lines
+    #[arg(
+        short = 'b',
+        long = "number-nonblank",
+        default_value_t = false,
+        required = false
+    )]
     number_nonblank_lines: bool,
-}
-
-pub fn get_args() -> MyResult<Config> {
-    let matches = App::new("rcat")
-        .version("0.1.0")
-        .author("Takeweb <takeweb@mac.com>")
-        .about("Rust cat")
-        .arg(
-            Arg::with_name("files")
-                .value_name("FILE")
-                .help("Input file(s)")
-                .multiple(true)
-                .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("number_lines")
-                .short("n")
-                .long("number")
-                .help("Number lines")
-                .takes_value(false)
-                .conflicts_with("number_nonblank_lines"),
-        )
-        .arg(
-            Arg::with_name("number_nonblank_lines")
-                .short("b")
-                .long("number-nonblank")
-                .help("Number nonblank lines")
-                .takes_value(false),
-        )
-        .get_matches();
-
-    Ok(Config {
-        files: matches.values_of_lossy("files").unwrap(),
-        number_lines: matches.is_present("number_lines"),
-        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
-    })
 }
 
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
@@ -55,17 +34,18 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     }
 }
 
-pub fn run(config: Config) -> MyResult<()> {
-    for filename in config.files {
+pub fn run() -> MyResult<()> {
+    let args = Args::parse();
+    for filename in args.files {
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
             Ok(file) => {
                 let mut last_num = 0;
                 for (line_num, line_result) in file.lines().enumerate() {
                     let line = line_result?;
-                    if config.number_lines {
+                    if args.number_lines {
                         println!("{:6}\t{}", line_num + 1, line);
-                    } else if config.number_nonblank_lines {
+                    } else if args.number_nonblank_lines {
                         if line.is_empty() {
                             println!();
                         } else {
